18b97004d2ada3aff13ba675a0f65297b7ef5fc4
EloToJaa <elotoja@protonmail.com>
2026-09-11 09:26:53 +0200

    chore(ci): check formatting build and unit tests

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -16,6 +20,7 @@
       nixpkgs,
       flake-utils,
       naersk,
+      treefmt-nix,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -23,8 +28,15 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         naerskLib = pkgs.callPackage naersk { };
+        treefmt = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.nixfmt.enable = true;
+          programs.rustfmt.enable = true;
+        };
       in
       {
+        checks.formatting = treefmt.config.build.check self;
+
         packages.default = naerskLib.buildPackage {
           pname = "ri-agent";
           version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).workspace.package.version;
@@ -73,7 +85,7 @@
           RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
         };
 
-        formatter = pkgs.nixfmt;
+        formatter = treefmt.config.build.wrapper;
       }
     );
 }
